e02a4b34e313b1cf5cafe068411ce7e3628d5842
Aaryatech <code@atsinfosoft.com>
2018-08-28 15:04:43 +0530

    cons//Sac

---
+++ src/main/java/com/ats/msadmin/common/Constants.java
@@ -0,0 +1,15 @@
+package com.ats.msadmin.common;
+
+public class Constants {
+
+	public static String url="http://localhost:8082/";
+	
+	public static int CAT_FILE_TYPE=1;
+	
+	public static int ITEM_FILE_TYPE=2;
+	
+	public static final String CAT_FOLDER = "https://www.gstatic.com/";
+
+	public static final String ITEM_FOLDER = "/home/ats-11/item/";
+	
+}
